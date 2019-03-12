7326b64ca319e0d468a44af8380123c273edf73b
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2019-03-12 10:34:04 +0800

    Alphabetical sort

---
+++ all.js
@@ -0,0 +1,223 @@
+const AishiteCard = document.querySelector('#aishite-card')
+const LarawanCard = document.querySelector('#larawan-card')
+const BalangigaCard = document.querySelector('#balangiga-card')
+const BalerCard = document.querySelector('#baler-card')
+const ThirdWorldCard = document.querySelector('#thirdWorld-card')
+const Batch81Card = document.querySelector('#batch81-card')
+const BonifacioCard = document.querySelector('#bonifacio-card')
+const Dekada70Card = document.querySelector('#dekada70-card')
+const GoyoCard = document.querySelector('#goyo-card')
+const GiveUpCard = document.querySelector('#giveUp-card')
+const LunaCard = document.querySelector('#luna-card')
+const RizalCard = document.querySelector('#rizal-card')
+const LiwayCard = document.querySelector('#liway-card')
+const GamuGamoCard = document.querySelector('#gamuGamo-card')
+const MLCard = document.querySelector('#ml-card')
+const NinoyCard = document.querySelector('#ninoy-card')
+const OroCard = document.querySelector('#oro-card')
+const SakayCard = document.querySelector('#sakay-card')
+const SisterCard = document.querySelector('#sister-card')
+const WalangDiyosCard = document.querySelector('#walangDiyos-card')
+
+// const Alphabetical = document.querySelector('#alphabetical')
+// const DateProduced = document.querySelector('#dateProduced')
+var SortValue = document.querySelector('#sortSelect').value
+var MRTCBValue = document.querySelector('#mrtcbSelect').value
+var StreamingValue = document.querySelector('#streamingSelect').value
+
+// function sortFunction() {
+//   if (SortValue === 1) {
+//     Alphabetical.style.display = "grid"
+//     DateProduced.style.display = "none"
+//     console.log(SortValue)
+//   } else {
+//     DateProduced.style.display = "grid"
+//     Alphabetical.style.display = "none"
+//     console.log(SortValue)
+//   }
+// }
+
+function familyDynamics() {
+  AishiteCard.style.display = 'none'
+  LarawanCard.style.display = 'block'
+  BalangigaCard.style.display = 'block'
+  BalerCard.style.display = 'none'
+  ThirdWorldCard.style.display = 'none'
+  Batch81Card.style.display = 'none'
+  BonifacioCard.style.display = 'none'
+  Dekada70Card.style.display = 'block'
+  GoyoCard.style.display = 'none'
+  GiveUpCard.style.display = 'none'
+  LunaCard.style.display = 'none'
+  RizalCard.style.display = 'none'
+  LiwayCard.style.display = 'block'
+  GamuGamoCard.style.display = 'block'
+  MLCard.style.display = 'none'
+  NinoyCard.style.display = 'none'
+  OroCard.style.display = 'block'
+  SakayCard.style.display = 'none'
+  SisterCard.style.display = 'none'
+  WalangDiyosCard.style.display = 'block'
+}
+
+function nationBuilding() {
+  AishiteCard.style.display = 'none'
+  LarawanCard.style.display = 'none'
+  BalangigaCard.style.display = 'none'
+  BalerCard.style.display = 'none'
+  ThirdWorldCard.style.display = 'block'
+  Batch81Card.style.display = 'none'
+  BonifacioCard.style.display = 'block'
+  Dekada70Card.style.display = 'none'
+  GoyoCard.style.display = 'block'
+  GiveUpCard.style.display = 'none'
+  LunaCard.style.display = 'block'
+  RizalCard.style.display = 'block'
+  LiwayCard.style.display = 'none'
+  GamuGamoCard.style.display = 'none'
+  MLCard.style.display = 'none'
+  NinoyCard.style.display = 'block'
+  OroCard.style.display = 'none'
+  SakayCard.style.display = 'block'
+  SisterCard.style.display = 'none'
+  WalangDiyosCard.style.display = 'none'
+}
+
+function humanRights() {
+  AishiteCard.style.display = 'none'
+  LarawanCard.style.display = 'none'
+  BalangigaCard.style.display = 'none'
+  BalerCard.style.display = 'none'
+  ThirdWorldCard.style.display = 'none'
+  Batch81Card.style.display = 'block'
+  BonifacioCard.style.display = 'none'
+  Dekada70Card.style.display = 'block'
+  GoyoCard.style.display = 'none'
+  GiveUpCard.style.display = 'block'
+  LunaCard.style.display = 'none'
+  RizalCard.style.display = 'none'
+  LiwayCard.style.display = 'block'
+  GamuGamoCard.style.display = 'none'
+  MLCard.style.display = 'block'
+  NinoyCard.style.display = 'none'
+  OroCard.style.display = 'block'
+  SakayCard.style.display = 'none'
+  SisterCard.style.display = 'none'
+  WalangDiyosCard.style.display = 'block'
+}
+
+
+function documentary() {
+  AishiteCard.style.display = 'none'
+  LarawanCard.style.display = 'none'
+  BalangigaCard.style.display = 'none'
+  BalerCard.style.display = 'none'
+  ThirdWorldCard.style.display = 'none'
+  Batch81Card.style.display = 'none'
+  BonifacioCard.style.display = 'none'
+  Dekada70Card.style.display = 'none'
+  GoyoCard.style.display = 'none'
+  GiveUpCard.style.display = 'block'
+  LunaCard.style.display = 'none'
+  RizalCard.style.display = 'none'
+  LiwayCard.style.display = 'none'
+  GamuGamoCard.style.display = 'none'
+  MLCard.style.display = 'none'
+  NinoyCard.style.display = 'block'
+  OroCard.style.display = 'none'
+  SakayCard.style.display = 'none'
+  SisterCard.style.display = 'none'
+  WalangDiyosCard.style.display = 'none'
+}
+
+function bioPic() {
+  AishiteCard.style.display = 'none'
+  LarawanCard.style.display = 'none'
+  BalangigaCard.style.display = 'none'
+  BalerCard.style.display = 'none'
+  ThirdWorldCard.style.display = 'block'
+  Batch81Card.style.display = 'none'
+  BonifacioCard.style.display = 'block'
+  Dekada70Card.style.display = 'none'
+  GoyoCard.style.display = 'block'
+  GiveUpCard.style.display = 'none'
+  LunaCard.style.display = 'block'
+  RizalCard.style.display = 'block'
+  LiwayCard.style.display = 'none'
+  GamuGamoCard.style.display = 'none'
+  MLCard.style.display = 'none'
+  NinoyCard.style.display = 'none'
+  OroCard.style.display = 'none'
+  SakayCard.style.display = 'block'
+  SisterCard.style.display = 'none'
+  WalangDiyosCard.style.display = 'none'
+}
+
+function colonialism() {
+  AishiteCard.style.display = 'block'
+  LarawanCard.style.display = 'none'
+  BalangigaCard.style.display = 'block'
+  BalerCard.style.display = 'none'
+  ThirdWorldCard.style.display = 'none'
+  Batch81Card.style.display = 'none'
+  BonifacioCard.style.display = 'none'
+  Dekada70Card.style.display = 'none'
+  GoyoCard.style.display = 'none'
+  GiveUpCard.style.display = 'none'
+  LunaCard.style.display = 'none'
+  RizalCard.style.display = 'block'
+  LiwayCard.style.display = 'none'
+  GamuGamoCard.style.display = 'block'
+  MLCard.style.display = 'none'
+  NinoyCard.style.display = 'none'
+  OroCard.style.display = 'none'
+  SakayCard.style.display = 'block'
+  SisterCard.style.display = 'none'
+  WalangDiyosCard.style.display = 'none'
+}
+
+function women() {
+  AishiteCard.style.display = 'block'
+  LarawanCard.style.display = 'none'
+  BalangigaCard.style.display = 'none'
+  BalerCard.style.display = 'none'
+  ThirdWorldCard.style.display = 'none'
+  Batch81Card.style.display = 'none'
+  BonifacioCard.style.display = 'none'
+  Dekada70Card.style.display = 'none'
+  GoyoCard.style.display = 'none'
+  GiveUpCard.style.display = 'none'
+  LunaCard.style.display = 'none'
+  RizalCard.style.display = 'none'
+  LiwayCard.style.display = 'block'
+  GamuGamoCard.style.display = 'block'
+  MLCard.style.display = 'none'
+  NinoyCard.style.display = 'none'
+  OroCard.style.display = 'block'
+  SakayCard.style.display = 'none'
+  SisterCard.style.display = 'block'
+  WalangDiyosCard.style.display = 'none'
+}
+
+function politics() {
+  AishiteCard.style.display = 'none'
+  LarawanCard.style.display = 'none'
+  BalangigaCard.style.display = 'none'
+  BalerCard.style.display = 'none'
+  ThirdWorldCard.style.display = 'none'
+  Batch81Card.style.display = 'none'
+  BonifacioCard.style.display = 'block'
+  Dekada70Card.style.display = 'block'
+  GoyoCard.style.display = 'block'
+  GiveUpCard.style.display = 'none'
+  LunaCard.style.display = 'block'
+  RizalCard.style.display = 'none'
+  LiwayCard.style.display = 'none'
+  GamuGamoCard.style.display = 'none'
+  MLCard.style.display = 'block'
+  NinoyCard.style.display = 'block'
+  OroCard.style.display = 'block'
+  SakayCard.style.display = 'block'
+  SisterCard.style.display = 'block'
+  WalangDiyosCard.style.display = 'none'
+}
